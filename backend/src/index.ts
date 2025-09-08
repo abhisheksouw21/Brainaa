b0313af61d266f4a96a7de2bbaa01440fed889bf
basharkhan7776 <basharkhan7776@gmail.com>
2025-09-08 12:25:03 +0530

    Update for vercel be deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,45 +12,32 @@ import { Signin, Signup } from "./routes/auth";
 import { DeleteContent, GetContent, PostContent, PutContent } from "./routes/content";
 import { GetShareBrain, PostShareBrain } from "./routes/brain";
 
-const numCPUs = os.cpus().length;
 
-if (cluster.isPrimary) {
-    console.log(`Primary process ${process.pid} is running`);
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died. Spawning a new one...`);
-        cluster.fork();
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+        origin: [frontendUrl, "http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({
+        message: `Brainly backend process: ${process.pid}`
     });
-} else {
-    const app = express();
-    app.use(express.json());
-    app.use(
-        cors({
-            origin: ["https://app-brainly.vercel.app"],
-            methods: ["GET", "POST", "PUT", "DELETE"],
-            credentials: true,
-        })
-    );
+});
 
-    app.get("/", (req, res) => {
-        res.json({
-            message: `Brainly backend process: ${process.pid}`
-        });
-    });
+app.post("/api/v1/signup", Signup);
+app.post("/api/v1/signin", Signin);
+app.post("/api/v1/content", userMiddleware, PostContent);
+app.get("/api/v1/content", userMiddleware, GetContent);
+app.put("/api/v1/content", userMiddleware, PutContent);
+app.delete("/api/v1/content", userMiddleware, DeleteContent)
+app.post("/api/v1/brain/share", userMiddleware, PostShareBrain);
+app.get("/api/v1/brain/:shareLink", GetShareBrain);
 
-    app.post("/api/v1/signup", Signup);
-    app.post("/api/v1/signin", Signin);
-    app.post("/api/v1/content", userMiddleware, PostContent);
-    app.get("/api/v1/content", userMiddleware, GetContent);
-    app.put("/api/v1/content", userMiddleware, PutContent);
-    app.delete("/api/v1/content", userMiddleware, DeleteContent)
-    app.post("/api/v1/brain/share", userMiddleware, PostShareBrain);
-    app.get("/api/v1/brain/:shareLink", GetShareBrain);
-
-    app.listen(3000, () => {
-        console.log(`Worker ${process.pid} started and listening on port 3000`);
-    });
-}
+app.listen(3000, () => {
+    console.log(`Worker started and listening on port 3000`);
+});
