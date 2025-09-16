cbc67013347ea42fccf90204714935ebab7514ed
basharkhan7776 <basharkhan7776@gmail.com>
2025-09-16 22:51:38 +0530

    Deploying to server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import cluster from 'cluster';
+import os from 'os';
 import express from "express";
 import { ContentModel, LinkModel, UserModel } from "./db";
 import { JWT_PASSWORD, frontendUrl } from "./config";
@@ -10,28 +12,45 @@ import { Signin, Signup } from "./routes/auth";
 import { DeleteContent, GetContent, PostContent, PutContent } from "./routes/content";
 import { GetShareBrain, PostShareBrain } from "./routes/brain";
 
+const numCPUs = os.cpus().length;
 
-const app = express();
-app.use(express.json());
-// app.use(
-//     cors({
-//         origin: [frontendUrl, "http://localhost:5173"],
-//         methods: ["GET", "POST", "PUT", "DELETE"],
-//         credentials: true,
-//     })
-// );
+if (cluster.isPrimary) {
+    console.log(`Primary process ${process.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Spawning a new one...`);
+        cluster.fork();
+    });
+} else {
+    const app = express();
+    app.use(express.json());
+    app.use(
+        cors({
+            origin: ["https://app-brainly.vercel.app/"],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true,
+        })
+    );
 
-app.get("/", (req, res) => {
-    res.send("Brainly server is running");
-});
+    app.get("/", (req, res) => {
+        res.json({
+            message: `Brainly backend process: ${process.pid}`
+        });
+    });
 
-app.post("/api/v1/signup", Signup);
-app.post("/api/v1/signin", Signin);
-app.post("/api/v1/content", userMiddleware, PostContent);
-app.get("/api/v1/content", userMiddleware, GetContent);
-app.put("/api/v1/content", userMiddleware, PutContent);
-app.delete("/api/v1/content", userMiddleware, DeleteContent)
-app.post("/api/v1/brain/share", userMiddleware, PostShareBrain);
-app.get("/api/v1/brain/:shareLink", GetShareBrain);
+    app.post("/api/v1/signup", Signup);
+    app.post("/api/v1/signin", Signin);
+    app.post("/api/v1/content", userMiddleware, PostContent);
+    app.get("/api/v1/content", userMiddleware, GetContent);
+    app.put("/api/v1/content", userMiddleware, PutContent);
+    app.delete("/api/v1/content", userMiddleware, DeleteContent)
+    app.post("/api/v1/brain/share", userMiddleware, PostShareBrain);
+    app.get("/api/v1/brain/:shareLink", GetShareBrain);
 
-export default app;
+    app.listen(3000, () => {
+        console.log(`Worker ${process.pid} started and listening on port 3000`);
+    });
+}
